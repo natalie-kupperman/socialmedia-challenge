a6ced1625a3c41b324bf94c9d1849fb858e94842
Natalie Kramer <natalie.kramer@mac.com>
2018-03-15 10:36:24 -0500

    Added files

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "na26gcyBvUucxrtuwPvq393cR"
+consumer_secret = "vDmwAIylwMNmn8iUt2HdtX5hGOKgH1lc9qvFJddGDbsCjphaQ2"
+access_token = "971513164104982530-YIKoyeBRGpbndcXLZ5GHdAV1vIfLUEX"
+access_token_secret = "jSgFEtFSykJgYqasUZRO1sQEl5EgnF9TCbziAjj8C2GAi"
